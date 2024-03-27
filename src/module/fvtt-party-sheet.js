7de4d92776efa7bead6572ee39b378a11c0bd709
Eddie Dover <ed@eddiedover.dev>
2024-03-26 17:22:24 -0700

    added more tests

--- src/module/fvtt-party-sheet.js
+++ src/module/fvtt-party-sheet.js
@@ -1,31 +1,9 @@
 /* eslint-disable no-undef */
 import { registerSettings } from "./app/settings.js";
 import { PartySheetForm } from "./app/party-sheet.js";
-import { addCustomSystem, toProperCase } from "./utils.js";
+import { log, loadSystemTemplates, toProperCase, areTemplatesLoaded } from "./utils.js";
 
 let currentPartySheet = null;
-let templatesLoaded = false;
-
-/**
- *
- * @param {any} message The message to send to console.log
- */
-function log(message) {
-  console.log("fvtt-party-sheet | ", message);
-}
-
-/**
- * Checks if the current environment is ForgeVTT
- * @returns {boolean} True if the current environment is ForgeVTT
- */
-function isForgeVTT() {
-  // @ts-ignore
-  if (typeof ForgeVTT === "undefined") {
-    return false;
-  }
-  // @ts-ignore
-  return ForgeVTT.usingTheForge;
-}
 
 // @ts-ignore
 Handlebars.registerHelper("hccontains", function (needle, haystack, options) {
@@ -187,69 +165,8 @@ function togglePartySheet() {
   }
 }
 
-/**
- * Load all the user-provided templates for systems
- * @param {string} path The path to the template
- * @returns {Promise<void>} A promise that resolves when the template is loaded
- */
-async function loadSystemTemplate(path) {
-  try {
-    const templateName = path.split("/").pop().split(".")[0];
-    log(`Loading template: ${templateName}`);
-    const template = JSON.parse(await fetch(path).then((r) => r.text()));
-    if (template.name && template.author && template.system && template.rows) {
-      console.log(`${path} - Good Template`);
-      addCustomSystem(template);
-    } else {
-      console.log(`${path} - Bad Template`);
-    }
-  } catch (e) {
-    console.log(`${path} - Failed to Load. See error below.`);
-    console.error(e);
-  }
-}
-
-/**
- * Load all the user-provided templates for systems
- */
-async function loadSystemTemplates() {
-  // Look inside the "partysheets" folder. Any JSON file inside should be loaded
-  const templatePaths = [];
-  // @ts-ignore
-
-  let assetPrefix = "data";
-
-  if (isForgeVTT()) {
-    console.log("Detected ForgeVTT");
-    // @ts-ignore
-    assetPrefix = ForgeVTT.ASSETS_LIBRARY_URL_PREFIX + (await ForgeAPI.getUserId()) + "/";
-  }
-
-  try {
-    // @ts-ignore
-    await FilePicker.createDirectory(assetPrefix, "partysheets"); //, { bucket: "public" }
-  } catch (e) {
-    console.log("Failed creating PartySheets directory. It probably already exists.");
-  }
-
-  // @ts-ignore
-  const templateFiles = await FilePicker.browse(assetPrefix, "partysheets"); // `modules/${MODULE_NAME}/templates`);
-
-  templateFiles.files.forEach((file) => {
-    if (file.endsWith(".json")) {
-      templatePaths.push(file);
-    }
-  });
-
-  for (const path of templatePaths) {
-    await loadSystemTemplate(path);
-  }
-
-  templatesLoaded = true;
-}
-
 const showButton = () => {
-  if (templatesLoaded) {
+  if (areTemplatesLoaded()) {
     // @ts-ignore
     const button = $(`<li class="control-tool "
       data-tool="PartySheet"
@@ -283,7 +200,7 @@ Hooks.on("ready", async () => {
 
   // @ts-ignore
   if (game.user.isGM) {
-    if (!templatesLoaded) {
+    if (!areTemplatesLoaded()) {
       log("Loading templates");
       await loadSystemTemplates();
     }
