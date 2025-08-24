34dac15b641f2aa69fdeee8d3fdeb7adabf96f74
Eddie Dover <ed@eddiedover.dev>
2025-08-23 17:30:06 -0700

    chore: playwright updates

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -37,15 +37,15 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
-
     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
+
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
     // {
     //   name: "webkit",
