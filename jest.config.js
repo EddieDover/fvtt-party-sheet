c3284e657ab64604136c1257406505489483c31c
Eddie Dover <ed@eddiedover.dev>
2025-09-05 06:21:08 -0700

    updated testing convig

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
 export default {
   testEnvironment: "jsdom",
   testMatch: ["**/*.test.js"],
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "src/module/vendor/dompurify.es.js",
-    "src/module/utils/dompurify-sanitizer.js",
-    "index.js",
-    "test-mocks.js",
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/module/vendor/**",
+    "!src/module/utils/dompurify-sanitizer.js",
+    "!src/module/app/**",
+    "!src/module/parsing/index.js",
+    "!src/module/parsing/processors/index.js",
+    "!src/module/fvtt-party-sheet.js",
+    "!src/module/typeDefs.js",
+    "!**/*.test.js",
+    "!**/test-mocks.js",
+    "!**/node_modules/**",
   ],
+  coveragePathIgnorePatterns: ["/node_modules/"],
   coverageThreshold: {
     global: {
-      lines: 80,
+      lines: 95,
     },
   },
 };
